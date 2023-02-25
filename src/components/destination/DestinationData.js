237bbe24de782a757761a836688db9f5322f3a2f
Jep Zekiel <jezedevkiel21@gmail.com>
2023-02-25 11:37:01 +0800

    Final Home Additional

--- src/components/destination/DestinationData.js
+++ src/components/destination/DestinationData.js
@@ -4,15 +4,17 @@ import "../../styles/destination.css";
 class DestinationData extends Component{
     render(){
         return (
-            <div className="first-des">
+            <div className={this.props.className}>
             <div className="des-text">
               <h2> {this.props.heading} </h2>
               <p> {this.props.text} </p>
             </div>
 
             <div className="image">
-              <img alt="img" src={this.props.Baby1} />
-              <img alt="img"  src={this.props.Baby2} />
+              <img alt="img" src={this.props.img1} />
+              <img alt="img" src={this.props.img2} />
+              <img alt="img" src={this.props.img3} />
+              <img alt="img" src={this.props.img4} />
             </div>
 
           </div>
