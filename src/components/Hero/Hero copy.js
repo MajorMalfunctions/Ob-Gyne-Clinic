2fda53586c81069ffbe3283165d55211c9559428
Jep Zekiel <jezedevkiel21@gmail.com>
2023-03-01 06:46:28 +0800

    Add Animation

--- src/components/Hero/Hero copy.js
+++ src/components/Hero/Hero copy.js
@@ -1,19 +1,23 @@
 import React, {useState } from 'react';
 import '../../styles/hero.css';
-import Modal from 'react-modal';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 function Hero (props) {
-  const [ openModal, setOpenModal] = useState(false);
+    const [ name, setName] = useState("");
+    const [ contact, setContact] = useState("");
+    const [ time, setTime] = useState("");
+    const [ date, setDate] = useState("");
+    const [active, setActive] = useState(false);
 
-  const setOpenModalToTrue = () => {
-    setOpenModal(true)
-  }
+    const handleBooking = (e) => {
+      e.preventDefault();
+      setActive(!active);
 
-  const setOpenModalToFalse = () => {
-    setOpenModal(false)
-  }
+      alert('Booking Added!');
+      console.log('Booking Added!');
+    }
+
 
   return (
     <>
@@ -26,31 +30,66 @@ function Hero (props) {
         <div className="hero-text">
           <h1>{props.title}</h1>
           <p>{props.text}</p>
-          <Popup trigger=
-                {<button className="btnClass"> Online Booking </button>}
+          {props.buttonText}
+          <a>
+            <Popup trigger=
+                {<button className={props.btnClass}> Online Booking </button>}
                  nested
                  modal
                  closeOnDocumentClick
                  position="center"
                  >
-                {
-                  close => (
                     <div className="form-container">
                     <h1>Book Now!</h1>
                       <form>
                          {/* <i className="fa-regular fa-user icon fa-2x"></i> */}
-                         <input type="text" placeholder="Name" />
+                         <label>Full Name:</label>
+                         <input
+                          name="name"
+                          id="name"
+                          type="text"
+                          placeholder="Name"
+                          value={name}
+                          onChange={e => setName(e.target.value)}
+                        />
+                        {/* <i class="fa-regular fa-phone icon fa-2x"></i> */}
+                        <label>Phone Number:</label>
+                         <input
+                           id="contact"
+                           name="contact"
+                           type="number"
+                           placeholder="Phone number"
+                           value={contact}
+                           onChange={e => setContact(e.target.value)}
+                          />
                          {/* <i class="fa-regular fa-clock icon fa-2x"></i> */}
-                         <input type="time" placeholder="Time" />
+                         <label>Time:</label>
+                         <input
+                           id="time"
+                           name="time"
+                           type="time"
+                           placeholder="Time"
+                           value={time}
+                           onChange={e => setTime(e.target.value)}
+                          />
                          {/* <i class="fa-regular fa-calendar icon fa-2x"></i> */}
-                         <input type="date" placeholder="Date" />
+                         <label>Date:</label>
+                         <input
+                            id="date"
+                            name="date"
+                            type="date"
+                            placeholder="Date"
+                            value={date}
+                            onChange={e => setDate(e.target.value)}
+                          />
                         <br />
-                        <button>Book</button>
-                      </form>
+                      </form><br />
+                        <button onClick={handleBooking}>Book{' '}{' '}{' '}<i class="fa-solid fa-paper-plane"> </i></button>
+                        <br />
+                        { active ? `Hello, ${name} Booked ${time} : ${date}` : ""}
                 </div>
-                  )
-                }
           </Popup>
+          </a>
         </div>
         <div>
     </div>
