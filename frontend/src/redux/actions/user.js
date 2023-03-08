1eb6d999b08f3eb2901d56d2fe742c48fa08b76a
Jep Zekiel <jezedevkiel21@gmail.com>
2023-03-09 06:26:41 +0800

    Add Booking Redux Hooks

--- frontend/src/redux/actions/user.js
+++ frontend/src/redux/actions/user.js
@@ -11,7 +11,17 @@ const logOut = () => {
     }
 }
 
+export const addNinjaAction = (ninja) => ({
+	type: "ADD_NINJA",
+	payload: ninja,
+});
+
+export const deleteNinjaAction = (id) => {
+	return { type: "DELETE_NINJA", payload: id };
+};
+
 export default {
     setUser,
     logOut
 }
+
--- frontend/src/redux/reducers/user.js
+++ frontend/src/redux/reducers/user.js
@@ -1,20 +1,30 @@
-const currentUser = (state = {}, action) => {
-    switch(action.type){
-        case "SET_USER":
-            return {
-                ...state,
-                user: action.payload,
-                loggedIn: true
-            }
-        case "LOG_OUT":
-            return {
-                ...state,
-                user: {},
-                loggedIn: false
-            }
-        default:
-            return state
-    }
-}
+// const currentUser = (state = { type, payload}, action) => {
+//     switch(action.type){
+//         case "SET_USER":
+//             return {
+//                 ...state,
+//                 user: action.payload,
+//                 loggedIn: true
+//             }
+//         case "LOG_OUT":
+//             return {
+//                 ...state,
+//                 user: {},
+//                 loggedIn: false
+//             }
+//             case "ADD_NINJA":
+//                 return {
+//                     ...state,
+//                     ninjas: [...state.ninjas, payload],
+//                 };
+//             case "DELETE_NINJA":
+//                 return {
+//                     ...state,
+//                     ninjas: state.ninjas.filter((ninja) => ninja.id !== payload),
+//                 };
+//         default:
+//             return state
+//     }
+// }
 
-export default currentUser;
+// export default currentUser;
