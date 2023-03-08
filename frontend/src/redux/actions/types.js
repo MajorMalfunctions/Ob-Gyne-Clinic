665a43bb46ff32926d60836fe85912a35059d079
Jep Zekiel <jezedevkiel21@gmail.com>
2023-03-09 03:59:05 +0800

    ADD Redux Auth

--- frontend/src/redux/actions/types.js
+++ frontend/src/redux/actions/types.js
@@ -10,3 +10,6 @@ export const LOGOUT = "LOGOUT";
 
 export const SET_MESSAGE = "SET_MESSAGE";
 export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
+
+export const SUCCESS_MESSAGE = "ALERT_SUCCESS";
+export const ERROR_MESSAGE = "ALERT_ERROR";
