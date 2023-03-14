9caa4f17c9d4e319851175230f04f69cdb8c80d1
Jep Zekiel <jezedevkiel21@gmail.com>
2023-03-14 12:22:07 +0800

    Additional Updates

---
+++ frontend/src/screens/Dashboard.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+const Dashboard = () => {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    navigate("/login");
+  }
+
+  return (
+    <div>
+      <h1>Welcome To Dashboard</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  )
+}
+
+export default Dashboard
+
+
+
+
+
+
+
+
+// import { useEffect, useState } from "react";
+// import { Navigate } from "react-router-dom";
+
+// const Dashboard = () => {
+//   const [ authenticated, setAuthenticated ] = useState(null);
+
+// useEffect(() => {
+//   const loggedInUser = localStorage.getItem("authenticated");
+//   if (loggedInUser) {
+//     setAuthenticated(loggedInUser);
+//     }
+//   }, []);
+
+//   if (!authenticated) {
+//     return <Navigate replace to="/dashboard" />;
+//   } else {
+//   return (
+//     <div>
+//         <h2>Welcome to Dashboard</h2>
+//     </div>
+//     );
+//   }
+// };
+
+// export default Dashboard;
+
+
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { getUser, removeUserSession } from '../utils/common';
+
+// const Dashboard = props => {
+//   const history = useNavigate();
+//   const user = getUser();
+
+//   // handle click event of logout button
+//   const handleLogout = () => {
+//     removeUserSession();
+//     history('/login');
+//   }
+
+//   return (
+//     <div>
+//       Welcome {user.name}!<br /><br />
+//       <input type="button" onClick={handleLogout} value="Logout" />
+//     </div>
+//   );
+// }
+
+// export default Dashboard;
---
+++ frontend/src/screens/Register copy.js
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate} from 'react-router-dom';
+import "../styles/register.css";
+import { toast } from 'react-toastify';
+<<<<<<<< HEAD:frontend/src/utils/Register copy.js
+import Form from './Forms';
+import Spinner from './Spinner'
+import Error from './Error'
+========
+import Form from '../utils/Forms';
+import Spinner from '../utils/Spinner'
+import Error from '../utils/Error'
+>>>>>>>> main:frontend/src/screens/Register copy.js
+
+const Register = () => {
+  const navigate = useNavigate();
+  // const { loading, userInfo, error } = useSelector((state) => state.auth)
+
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+       e.preventDefault();
+    await axios.post(
+      'http://localhost:5050/api/auth/signup',{
+        fullname: fullname,
+        email: email,
+        password: password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+      .then((res) =>  {
+        console.log(res.data)
+        console.log(res.message)
+        toast.success('success', { position: toast.POSITION.TOP_CENTER})
+        const token =  (fullname, email, password);
+        if (token) {
+          localStorage.setItem('accessToken', res.data.user)
+          localStorage.setItem('accessToken', res.data.accessToken)
+          navigate('/profile')
+        }
+        console.log(res.status)
+        console.log(res.data)
+
+        // if (res.status(400).send({  message: "Email Alreay Exists"})){
+        // }  else {
+        //   res.status(200).send({ message: 'success'})
+        //   console.log('success')
+        // }
+     })
+     .catch(error => {
+      //  toast.error('error')
+      alert('ERROR')
+      alert(error.message)
+      console.log(error)
+      console.log(error.message)
+      console.log(error.res.data.err.message)
+      navigate("/register");
+     })
+
+  }
+
+  return (
+    <div className="register-screen">
+    {/* {error && <Error>{error}</Error>} */}
+      <form onSubmit={handleRegister} className="register-screen_form">
+        <h3 className="register-screen_title">Register</h3>
+
+        <div className="form-group">
+          <label htmlFor="fullname">Fullname:</label>
+          <input
+            type="text"
+            required
+            id="name"
+            placeholder="Enter fullname"
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            required
+            id="email"
+            placeholder="Email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            required
+            id="password"
+            autoComplete="true"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {/* <button type="submit" className="btn btn-primary" disabled={loading}> */}
+        <button type="submit" className="btn btn-primary">
+          Register
+        </button>
+        {/* {loading ? <Spinner /> : 'Login'} */}
+        {/* {error && <div className="error">{error}</div>} */}
+        <br />
+        <br />
+        <p className="register-subtext">Already A Member?  <Link to="/login">Login Here!</Link></p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
